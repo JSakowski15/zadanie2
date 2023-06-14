337e40c87547abe042c7f0fcab3f47e3c76e8904
JSakowski15 <133519661+JSakowski15@users.noreply.github.com>
2023-06-14 21:02:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sakowski Jakub - Zadanie 2
+          Sakowski Jakub <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
